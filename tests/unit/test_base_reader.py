40c3248e23c0ed7087167b84f0af80c40e37dc74
volodymyr <zalorderon3331@gmail.com>
2024-02-03 14:02:52 +0200

    fixed tests

--- tests/unit/test_base_reader.py
+++
@@ -1,7 +0,0 @@
-"""Contains tests for the BaseReader class."""
-from py4phi.readers.base_readers.base_reader import BaseReader
-
-
-def test_base_reader():
-    assert len(BaseReader.supported_file_types) == 0
-
