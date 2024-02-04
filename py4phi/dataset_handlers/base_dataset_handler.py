11aebc19d58eed01fcf513017ab59211c51906f6
volodymyr <zalorderon3331@gmail.com>
2024-02-04 16:13:22 +0200

    added logic

--- py4phi/dataset_handlers/base_dataset_handler.py
+++ py4phi/dataset_handlers/base_dataset_handler.py
@@ -3,22 +3,26 @@ from abc import ABC, abstractmethod
 from typing import Any
 
 
-class BaseReader(ABC):
+class BaseDatasetHandler(ABC):
     """
     Abstract reader class.
 
     Defines basic reading functionality
-     to be implemented and extended by particular readers.
+     to be implemented and extended by particular dataset_handlers.
     """
 
     def __init__(self):
         self._df = None
+        self._writing_method = None
 
-    def _get_reading_method(self, file_type: str) -> callable:
-        method = {'CSV': self._read_csv, 'PARQUET': self._read_parquet}.get(file_type)
-        if not method:
+    def _get_interaction_methods(self, file_type: str) -> tuple[callable, callable]:
+        methods = {
+            'CSV': (self._read_csv, self._write_csv),
+            'PARQUET': (self._read_parquet, self._write_parquet)
+        }.get(file_type)
+        if not methods:
             raise NotImplementedError(f'No reading method for file type {file_type}')
-        return method
+        return methods
 
     @property
     def df(self):
@@ -58,8 +62,11 @@ class BaseReader(ABC):
         Returns: None
 
         """
-        reading_method = self._get_reading_method(file_type=file_type.upper())
+        reading_method, writing_method = self._get_interaction_methods(
+            file_type=file_type.upper()
+        )
         self._df = reading_method(path, **kwargs)
+        self._writing_method = writing_method
 
     @abstractmethod
     def _read_csv(self, path: str, *args, **kwargs) -> Any:
@@ -93,6 +100,59 @@ class BaseReader(ABC):
         Returns:
         -------
             None: Assigns dataframe to self._df property of the reader object.
+
+        """
+        pass
+
+    def write(self, df: Any, name: str, path: str, **kwargs):
+        """
+        Write file.
+
+        Args:
+        ----
+        df (Any): Dataframe to be saved.
+        name (str): Name of the resulting file.
+        path (str): path to save csv file.
+        kwargs: Additional key-value arguments.
+
+        Returns: None.
+
+        """
+        writing_method = self._writing_method or self._write_csv
+        writing_method(df, name, path, **kwargs)
+
+    @abstractmethod
+    def _write_csv(self, df: Any, name: str, path: str, *args, **kwargs) -> None:
+        """
+        Write csv file.
+
+        Args:
+        ----
+        df (Any): Dataframe to be saved.
+        name (str): Name of the resulting file.
+        path (str): path to save csv file.
+        args: Positional arguments supplied to the function.
+        kwargs: Additional key-value arguments.
+
+        Returns: None.
+
+        """
+        pass
+
+    @abstractmethod
+    def _write_parquet(self, df: Any, name: str,  path: str, *args, **kwargs) -> None:
+        """
+        Write parquet file.
+
+        Args:
+        ----
+        df (Any): Dataframe to be saved.
+        name (str): Name of the resulting file.
+        path (str): path to save parquet file.
+        args: Positional arguments supplied to the function.
+        kwargs: Additional key-value arguments.
+
+        Returns: None.
 
         """
         pass
---
+++ py4phi/dataset_handlers/pyspark_dataset_handler.py
@@ -0,0 +1,115 @@
+"""Module for reading file or dataframe of pyspark."""
+import os
+
+from pyspark.sql import SparkSession, DataFrame, types as t
+
+from py4phi.dataset_handlers.base_dataset_handler import BaseDatasetHandler
+from py4phi.dataset_handlers.pyspark_write_utils import copy_merge_into
+
+TMP_OUTPUT_DIR = "tmp-spark"
+# TODO ADD LOGGING, ADD CUSTOM EXCEPTIONS, ADD FILE FORMATS SUPPORT, ADD TESTS, ADD PERSONAL ALLOWED TYPES MAPPING # noqa: E501
+
+
+class PySparkDatasetHandler(BaseDatasetHandler):
+    """Class for reading in file or PySpark dataframe."""
+
+    def __init__(self):
+        super().__init__()
+        self._spark = SparkSession.builder.getOrCreate()
+
+    def _to_pyspark(self, df: DataFrame) -> DataFrame:
+        return df
+
+    def _read_csv(self, path: str, **kwargs) -> DataFrame:
+        """
+        Read csv file with PySpark.
+
+        Args:
+        ----
+        path (str): path to csv file.
+        **kwargs (dict): Key-value pairs to pass to Spark csv reading function.
+
+        Returns:
+        -------
+            None: Assigns dataframe to self._df property of the reader object.
+
+        """
+        return self._spark.read.csv(path, header=kwargs['header'], multiLine=True)
+
+    def _read_parquet(self, path: str, **kwargs) -> DataFrame:
+        """
+        Read parquet file with PySpark.
+
+        Args:
+        ----
+        path (str): path to parquet file.
+        **kwargs (dict): Key-value pairs to pass to Spark parquet reading function.
+
+        Returns:
+        -------
+            None: Assigns dataframe to self._df property of the reader object.
+        """
+        return self._spark.read.parquet(path)
+
+    def _write_csv(self, df: DataFrame, name: str, path: str, **kwargs) -> None:
+        """
+        Write csv file with PySpark.
+
+        Args:
+        ----
+        df (DataFrame): PySpark Dataframe to be saved.
+        name (str): Name of the resulting file.
+        path (str): path to save csv file.
+        kwargs: Additional key-value arguments.
+
+        Returns: None.
+
+        """
+        suffix = '.csv' if not path.endswith('.csv') else ''
+
+        headers = self._spark.createDataFrame(
+            data=[[f.name for f in df.schema.fields]],
+            schema=t.StructType(
+                [t.StructField(f.name, t.StringType(), False) for f in df.schema.fields]
+            ),
+        )
+        headers.write.csv(TMP_OUTPUT_DIR, mode='overwrite')
+        copy_merge_into(
+            self._spark,
+            TMP_OUTPUT_DIR,
+            os.path.join(path, name) + suffix,
+            delete_source=True,
+        )
+
+        df.write.csv(TMP_OUTPUT_DIR, **kwargs)
+
+        copy_merge_into(
+            self._spark,
+            TMP_OUTPUT_DIR,
+            os.path.join(path, name) + suffix,
+            delete_source=True,
+        )
+
+    def _write_parquet(self, df: DataFrame, name: str, path: str,  **kwargs) -> None:
+        """
+        Write parquet file.
+
+        Args:
+        ----
+        df (DataFrame): Dataframe to be saved.
+        name (str): Name of the resulting file.
+        path (str): path to save parquet file.
+        kwargs: Additional key-value arguments.
+
+        Returns: None.
+
+        """
+        suffix = '.parquet' if not path.endswith('.parquet') else ''
+        df.write.parquet(TMP_OUTPUT_DIR, **kwargs)
+
+        copy_merge_into(
+            self._spark,
+            TMP_OUTPUT_DIR,
+            os.path.join(path, name) + suffix,
+            delete_source=True,
+        )
