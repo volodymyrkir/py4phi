7799bbdd80d8039d4a91463ad49144c84223178f
volodymyr kiriushyn <99387760+volodymyrkir@users.noreply.github.com>
2024-02-15 00:09:57 +0200

    added pandas dataset handler and tests (#12)

---
+++ py4phi/dataset_handlers/pandas_dataset_handler.py
@@ -0,0 +1,117 @@
+"""Module for reading file or dataframe by Pandas."""
+import os
+from typing import override
+
+import pandas as pd
+
+from py4phi.dataset_handlers.base_dataset_handler import BaseDatasetHandler, PathOrStr
+
+
+class PandasDatasetHandler(BaseDatasetHandler):
+    """Class for reading in file or Pandas dataframe."""
+
+    @staticmethod
+    @override
+    def print_df(df: pd.DataFrame) -> None:
+        """
+        Print PySpark dataframe.
+
+        Args:
+        ----
+        df (DataFrame): PySpark dataframe.
+
+        Returns: None.
+
+        """
+        if not isinstance(df, pd.DataFrame):
+            raise ValueError('Non-Pandas DataFrame passed to PandasDatasetHandler')
+        pd.set_option('display.max_columns', None)
+        print(df.head(30))
+
+    @override
+    def _read_csv(self, path: PathOrStr, **kwargs) -> pd.DataFrame:
+        """
+        Read csv file with Pandas.
+
+        Args:
+        ----
+        path (str): path to csv file.
+        **kwargs (dict): Key-value pairs to pass to pandas csv reading function.
+
+        Returns:
+        -------
+            None: Assigns dataframe to self._df property of the handler object.
+
+        """
+        return pd.read_csv(path, engine='pyarrow', dtype_backend='pyarrow', **kwargs)
+
+    @override
+    def _read_parquet(self, path: PathOrStr, **kwargs) -> pd.DataFrame:
+        """
+        Read parquet file with Pandas.
+
+        Args:
+        ----
+        path (str): path to parquet file.
+        **kwargs (dict): Key-value pairs to pass to Pandas parquet reading function.
+
+        Returns:
+        -------
+            None: Assigns dataframe to self._df property of the handler object.
+        """
+        return pd.read_parquet(
+            path,
+            engine='pyarrow',
+            dtype_backend='pyarrow',
+            **kwargs
+        )
+
+    @override
+    def _write_csv(
+            self,
+            df: pd.DataFrame,
+            name: str,
+            path: PathOrStr,
+            **kwargs
+    ) -> None:
+        """
+        Write csv file with Pandas.
+
+        Args:
+        ----
+        df (DataFrame): Pandas Dataframe to be saved.
+        name (str): Name of the resulting file.
+        path (str): path to save csv file.
+        kwargs: Additional key-value arguments.
+
+        Returns: None.
+
+        """
+        suffix = '.csv' if not str(name).endswith('.csv') else ''
+
+        df.to_csv(os.path.join(path, name + suffix), index=False, **kwargs)
+
+    @override
+    def _write_parquet(
+            self,
+            df: pd.DataFrame,
+            name: str,
+            path: PathOrStr,
+            **kwargs
+    ) -> None:
+        """
+        Write parquet file.
+
+        Args:
+        ----
+        df (DataFrame): Dataframe to be saved.
+        name (str): Name of the resulting file.
+        path (str): path to save parquet file.
+        kwargs: Additional key-value arguments.
+
+        Returns: None.
+
+        """
+        suffix = '.parquet' if not str(path).endswith('.parquet') else ''
+
+        df.to_parquet(os.path.join(path, name + suffix), index=False, **kwargs)
